da450f12a9700541f6b095c6f4cef6d2db8eaa86
Tarzan-07 <tarunraghavsrikanth@gmail.com>
2026-04-15 22:51:32 +0530

    Add: CNN implementation

---
+++ model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+
+class SVNHCNN(nn.Module):
+    def __init__(self, num_layers):
+        # super().__init__(*args, **kwargs)
+
+        self.num_layer = num_layers
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(128*8*8, 512)
+        self.fc2 = nn.Linear(512, 10)
+
+        self.dropout = nn.Dropout(0.5)
+    
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool1(x)
+        x = self.dropout(x)
+        
+        x = F.relu(self.conv3(x))
+        x = self.pool2(x)
+        x = self.dropout(x)
+        
+        x = x.view(-1, 128 * 8 * 8) # Flatten
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
